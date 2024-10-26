8d3f66de58c1978495234b0238c98886fce9173b
PatatoBit <pathonscript@gmail.com>
2024-10-26 11:29:56 +0700

    layout polished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,30 @@
 	<br />
 
 	<div class="hero">
-		<p class="label">hi there, welcome to</p>
-		<h1>Pat's corner of the internet</h1>
-		<p>Pat create projects and hope they’re useful to at least a person</p>
+		<div class="hero-text">
+			<p class="label">hi there, welcome to</p>
+			<h1>Pat's corner of the internet</h1>
+			<p>stick around if you'd like, there are some fun stuffs here</p>
+		</div>
+
+		<img src="/pfp.jpeg" alt="Patato" />
 	</div>
 
 	<hr />
 </div>
+
+<style lang="scss">
+	.hero {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-wrap: wrap-reverse;
+		gap: 2rem;
+
+		img {
+			width: 7rem;
+			aspect-ratio: 1;
+			border-radius: 50%;
+		}
+	}
+</style>
