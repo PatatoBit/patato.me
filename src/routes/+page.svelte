527b7dc5b9de2b16859f1a4d3de029d4fd4f0fc5
PatatoBit <pathonscript@gmail.com>
2024-10-26 12:12:43 +0700

    font and spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,15 @@
 
 	<div class="hero">
 		<div class="hero-text">
-			<p class="label">hi there, welcome to</p>
-			<h1>Pat's corner of the internet</h1>
+			<div>
+				<p class="label">hi there, welcome to</p>
+				<h1>Pat's corner of the internet</h1>
+			</div>
 			<p>stick around if you'd like, there are some fun stuffs here</p>
 		</div>
 
 		<img src="/pfp.jpeg" alt="Patato" />
 	</div>
-
 	<hr />
 </div>
 
@@ -22,10 +23,20 @@
 		flex-wrap: wrap-reverse;
 		gap: 2rem;
 
+		.hero-text {
+			display: flex;
+			flex-direction: column;
+			gap: 0.1rem;
+		}
+
 		img {
 			width: 7rem;
 			aspect-ratio: 1;
 			border-radius: 50%;
+		}
+
+		h1 {
+			line-height: 0.5;
 		}
 	}
 </style>
